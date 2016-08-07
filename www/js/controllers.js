7b3899041c61f45a3cc328511f81bfc4b064cd70
Joonpyo Jun <jpjun8@gmail.com>
2016-08-06 23:38:55 -0400

    Pool Hall controller on dev + go back to home after deployed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,23 +18,13 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('deployDetailCtrl', function($scope, $stateParams, Deployments, $firebase, $ionicPopup) {
+.controller('deployDetailCtrl', function($scope, $stateParams, Deployments, $firebase, $ionicPopup, $state) {
   $scope.deploy = Deployments.get($stateParams.deployId); //deployment type: Technology, Pool Hall, etc.
-  //var BackHub = monaca.cloud.Collection("Technology");
-  //console.log(BackHub);
-   // Intialize the "[DEFAULT]" App
   $scope.input = {};
 
   var asset_name;
 
-  function getAllData() {
-      firebase.database().ref('Technology/').on('value', function (snapshot) {
-          // console.log(snapshot.val());
-      });
-  }
-
   function saveData(date, name, email, ai, asset, witid) {
-
     var data = {
       date: date,
       name: name,
@@ -45,15 +35,11 @@ angular.module('starter.controllers', [])
     };
 
     var key = firebase.database().ref().child('Technology').push().key;
-    console.log(key);
 
     var updates = {};
     updates['Technology/'+key] = data;
-    console.log('Updated');
     return firebase.database().ref().update(updates);
-
     //ENDS HERE
-
   }
 
   function customAlert(title, template) {
@@ -70,20 +56,19 @@ angular.module('starter.controllers', [])
       // alert('Try Scan QR Code Again');
     } else {
       saveData($scope.input.date, $scope.input.name, $scope.input.email, $scope.input.ai, asset, $scope.input.witid);
+      firebase.database().ref('Technology/Assets/' + asset_name + '/status').once('value', function(snapshot) {
+        if(snapshot.val() == 'IN') {
+          firebase.database().ref('Technology/Assets/' + asset_name).update({
+            status: 'OUT'
+          });
+        }
+      });
+      customAlert('HubBuddy says: ', 'Information saved on DB.');
+      $state.go('tab.Deployments');
+
     }
 
-    firebase.database().ref('Technology/Assets/' + asset_name + '/status').once('value', function(snapshot) {
-      if(snapshot.val() == 'IN') {
-        firebase.database().ref('Technology/Assets/' + asset_name).update({
-          status: 'OUT'
-        });
-      }
-    });
 
-  }
-
-  $scope.getAllData = function () {
-     getAllData();
   }
 
   $scope.scanBarcode = function() {
@@ -129,6 +114,42 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('poolCtrl', function($scope) {
+.controller('poolCtrl', function($scope, $firebase, $ionicPopup, $state, $stateParams, Deployments) {
+  $scope.deploy = Deployments.get($stateParams.deployId);
+  $scope.input = {};
+  var poolkeyArray = [];
+
+  function saveData(date, name, email, ai, witid) {
+    var data = {
+      date: date,
+      name: name,
+      email: email,
+      ai: ai,
+      witid: witid
+    };
+
+    var key = firebase.database().ref().child('Pool').push().key;
+    poolkeyArray.push(key);
+    //prob need to save into keyArray for future usage
+    //push -> adds a item in the end of array.
+
+    var updates = {};
+    updates['Pool/'+key] = data;
+    return firebase.database().ref().update(updates);
+    //ENDS HERE
+  }
+
+  function customAlert(title, template) {
+    var alertPop = $ionicPopup.alert({
+      title: title,
+      template: template
+    });
+  }
+
+  $scope.saveData = function() {
+    saveData($scope.input.date, $scope.input.name, $scope.input.email, $scope.input.ai, $scope.input.witid);
+    customAlert('HubBuddy says: ', 'Pool Hall info saved to DB.');
+  }
+
 
 });
