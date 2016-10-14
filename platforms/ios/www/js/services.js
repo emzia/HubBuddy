95a644ee578485a160f0dd918d12c35aa0f0c721
Emily Duong <duongh2@wit.edu>
2016-10-13 22:43:35 -0400

    Saving Changes in new branch

---
+++ platforms/ios/www/js/services.js
@@ -0,0 +1,73 @@
+angular.module('starter.services', [])
+
+
+.factory('Deployments', function () {
+    // Might use a resource here that returns a JSON array
+
+    var Deployments = [{
+        id: 0,
+        name: 'Technology, Sports, & Board Games',
+        lastText: 'Ask for their Wentworth ID!',
+        icon: 'img/deploy.png'
+    }, {
+        id: 1,
+        name: 'Pool Hall',
+        lastText: 'Who\'s going to win the challenge?!',
+        icon: 'img/pool.png'
+    }, {
+        id: 2,
+        name: 'Check-In & Return Assets',
+        lastText: 'Don\'t forget to return their ID!',
+        icon: 'img/checkin.png'
+    }];
+
+
+
+    return {
+        all: function () {
+            return Deployments;
+        },
+        remove: function (deploy) {
+            Deployments.splice(Deployments.indexOf(deploy), 1);
+        },
+        get: function (deployId) {
+            for (var i = 0; i < Deployments.length; i++) {
+                if (Deployments[i].id === parseInt(deployId)) {
+                    return Deployments[i];
+                }
+            }
+            return null;
+        }
+    };
+})
+
+.factory('Records', function() {
+  var Records = [{
+    id: 0,
+    name: 'Deployment Logs',
+    lastText: 'Technology, Sports, Board Games, and Pool Equipment',
+    icon: 'img/logs.png'
+  }, {
+    id: 1,
+    name: 'Room Count Logs',
+    lastText: 'Students hanging around Beatty ~',
+    icon: 'img/logs.png'
+  }];
+
+  return {
+    all: function() {
+      return Records;
+    },
+    remove: function(record) {
+      Records.splice(Records.indexOf(record), 1);
+    },
+    get: function(recordId) {
+      for(var i = 0; i < Records.length; i++) {
+        if(Records[i].id === parseInt(recordId)) {
+          return Records[i];
+        }
+      }
+      return null;
+    }
+  };
+});
